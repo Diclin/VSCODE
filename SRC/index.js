1c8bb034e84429f18611b2edbbdf6ea6d3b6e607
Diclin <daniel_claudio97@hotmail.com>
2025-01-20 02:19:34 +0100

    proyecto

---
+++ SRC/index.js
@@ -0,0 +1,12 @@
+const fn = () => {
+    const miString = "MA QUE COSSA"
+    let b = miString + "RUMBOIA"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "VIVA"
+    console.log(a)
+}
